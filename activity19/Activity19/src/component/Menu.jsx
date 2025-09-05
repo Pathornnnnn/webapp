e320bc8b0a35c0a840ee4daa7834e98e19755f89
Pathorn <pathornsomsaen@gmail.com>
2025-09-05 12:06:38 +0700

    build vote(),unvote() : add feature vote , unvote in menu component

--- activity19/Activity19/src/component/Menu.jsx
+++ activity19/Activity19/src/component/Menu.jsx
@@ -1,6 +1,44 @@
 import React from "react";
+import { useState } from 'react';
 import "./Menu.css";
 export default function Menudiv(info) {
+  const [count, setCount] = useState("MIN");
+  const Vote = () => {
+    if (count === 'MAX') {
+      alert('Cannot Vote more');
+      return;
+    }
+    if (count >= 9){
+      setCount('MAX');
+      return;
+    }
+    else{
+      if (count === 'MIN') {
+        setCount(1); // If count was 'MIN', reset to 1
+      }
+      else
+      setCount(count + 1); // Updates the 'count' state
+    }
+  };
+  
+  const Unvote = () => {
+    if (count === 'MIN') {
+      alert('Cannot Unvote');
+      return;
+    }
+    
+    else
+    if (count === 'MAX') {
+      setCount(9); // If count was 'MAX', reset to 9
+    } else{
+      if (count == 1){
+        setCount('MIN');
+        return;
+      }
+      setCount(count  -1); // Updates the 'count' state
+    }
+  };
+
   return (
     <div className="Menudiv">
       <div className="top">
@@ -14,9 +52,9 @@ export default function Menudiv(info) {
         </div>
       </div>
       <div className="bottom">
-        <button>Click to Vote</button>
-        <h2>MIN</h2>
-        <button>Click to Unvote</button>
+        <button onClick={Vote}>Click to Vote</button>
+        <h2>{count}</h2>
+        <button onClick={Unvote}>Click to Unvote</button>
       </div>
     </div>
   );
